dd3f8ac5ebf32366b2de51babcb4696072375b69
angelchrist3 <angelchristou3@gmail.com>
2025-09-07 20:35:06 -0400

    Update install.R

--- install.R
+++ install.R
@@ -1,8 +1,8 @@
-install.packages("readr")
+install.packages(c("readr", "tidyr", "ggplot2"))
 install.packages("tidyverse", dependencies = TRUE)
 install.packages("rmarkdown", dependencies = TRUE)
 install.packages("IRkernel", dependencies = TRUE)
 
 IRkernel::installspec(user = FALSE)
 
-install.packages(c("readr", "tidyr", "ggplot2"))
+
